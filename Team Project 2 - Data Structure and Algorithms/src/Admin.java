418ce48e7276e1279a48d5b6c09d1988a3ded900
FSMedina <medinafaizashifa@gmail.com>
2026-09-20 19:32:01 +0700

    Admin.java

--- Team Project 2 - Data Structure and Algorithms/src/Admin.java
+++ Team Project 2 - Data Structure and Algorithms/src/Admin.java
@@ -1,55 +1,26 @@
-import java.util.Scanner;
-
+/*
+ * Tugas Kelompok 2 - Data Structures and Algorithm Analysis
+ * Kelompok 2 | Kelas: [ISI KELAS]
+ * Anggota:
+ *  1. Julyon Putra Sannie      / 2902816550
+ *  2. Dede Rukmana             / 2902830366
+ *  3. Faiza Shifa Medina       / 2902826034
+ *  4. Bima Galaxy Putra        / 2902819464
+ *  5. Muhammad Taufiq Al Hakim / 2902820775
+ */
 public class Admin extends User {
     public Admin(String nama) {
         super(nama);
     }
 
-    // Method untuk menambahkan lagu ke playlist
-    // Jika array playlist penuh, maka buat playlist baru dengan ukuran 1 lebih besar dari playlist lama
-    // Lagu baru ditambahkan ke playlist baru
-    // Jika array playlist belum penuh, maka tambahkan lagu baru ke playlist lama
-    public Lagu[] tambahLagu(Lagu[] playlist, Scanner scanner) {
-        for (int i = 0; i < playlist.length; i++) {
-            // Melakukan pengecekan adakah slot kosong di array
-            if (playlist[i] == null) {
-                System.out.println("Masukan informasi lagu");
-                scanner.nextLine(); // Membersihkan newline character dari input sebelumnya
-                System.out.print("Judul: ");
-                String judul = scanner.nextLine();
-                System.out.print("Artis: ");
-                String artis = scanner.nextLine();
-                System.out.print("Durasi: ");
-                double durasi = scanner.nextDouble();
+    // Admin boleh menambah dan menghapus lagu
+    @Override
+    public boolean bolehMengubahPlaylist() {
+        return true;
+    }
 
-                System.out.println("Memasukan lagu ke playlist dengan index " + i);
-                System.out.println("Lagu berhasil ditambahkan");
-                playlist[i] = new Lagu(judul, artis, durasi);
-                return playlist;
-            }
-
-            // Cek apakah pointer index i sudah berada di akhir index array
-            if (playlist.length - 1 == i) {
-                // Beritahu bahwa array sudah penuh
-                // Kenapa cuman melakukan pengecekan pointer index berada di akhir index array
-                // Karena ketika ada slot akan masuk ke if statement diatas
-                // Dan tidak akan lanjut ke if statement ini karena sudah melakukan aksi return
-                System.out.println("Playlist sudah penuh, harap hapus lagu sebelum menambahkan lagu baru");
-                return playlist;
-            }
-        }
-
-//        System.out.println("Playlist penuh, harap hapus lagu terlebih dahulu untuk menambahkan lagu baru");
-//        return playlist;
-
-//        Lagu[] playlistBaru = new Lagu[playlist.length + 1];
-//
-//        for (int i = 0; i < playlist.length; i++) {
-//            playlistBaru[i] = playlist[i];
-//        }
-//
-//        playlistBaru[playlist.length] = laguBaru;
-//
-        return playlist;
+    @Override
+    public String tampilkanAkses() {
+        return "Admin - dapat menambah dan menghapus lagu";
     }
 }
